18d5c7a7b4f33412c4c960ffed82d32e82cb27e8
Cedrik Hoffmann <dev@choffmann.io>
2026-01-07 18:32:52 +0100

    chore: add nix flake for dev environment

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Development shell for green ecolution frontend";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs: (flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {};
+        };
+      in {
+        # devShells."x86_64-linux".default = import ./shell.nix { inherit pkgs; };
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nodejs_22
+            yarn
+            eslint
+            prettierd
+            yo
+          ];
+
+          shellHook = ''
+            ${pre-commit-check.shellHook}
+          '';
+        };
+      }
+    ));
+}
